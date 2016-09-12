3521408864dad744eeea8850a919875b9475a993
Christian Steimel <me@C14L.com>
2016-09-13 00:50:35 +0200

    Move translation JS files to the app, clean up some more unused files

---
+++ js/tr-language-en.js
@@ -0,0 +1,678 @@
+window.TR_LANGUAGE = [
+{
+"msgid": "find people", 
+"msgstr": "find people"
+}, 
+{
+"msgid": "talk", 
+"msgstr": "talk"
+}, 
+{
+"msgid": "inbox messages", 
+"msgstr": "inbox messages"
+}, 
+{
+"msgid": "new inbox messages", 
+"msgstr": "new inbox messages"
+}, 
+{
+"msgid": "matches", 
+"msgstr": "matches"
+}, 
+{
+"msgid": "friends", 
+"msgstr": "friends"
+}, 
+{
+"msgid": "viewed you", 
+"msgstr": "viewed you"
+}, 
+{
+"msgid": "your favorites", 
+"msgstr": "your favorites"
+}, 
+{
+"msgid": "blocked", 
+"msgstr": "blocked"
+}, 
+{
+"msgid": "pictures", 
+"msgstr": "pictures"
+}, 
+{
+"msgid": "view profile", 
+"msgstr": "view profile"
+}, 
+{
+"msgid": "All rights reserved.", 
+"msgstr": "All rights reserved."
+}, 
+{
+"msgid": "site rules", 
+"msgstr": "site rules"
+}, 
+{
+"msgid": "privacy", 
+"msgstr": "privacy"
+}, 
+{
+"msgid": "by all", 
+"msgstr": "by all"
+}, 
+{
+"msgid": "by matches", 
+"msgstr": "by matches"
+}, 
+{
+"msgid": "by friends", 
+"msgstr": "by friends"
+}, 
+{
+"msgid": "about me", 
+"msgstr": "about me"
+}, 
+{
+"msgid": "unread", 
+"msgstr": "unread"
+}, 
+{
+"msgid": "inbox", 
+"msgstr": "inbox"
+}, 
+{
+"msgid": "sent", 
+"msgstr": "sent"
+}, 
+{
+"msgid": "nobody there", 
+"msgstr": "nobody there"
+}, 
+{
+"msgid": "current topics", 
+"msgstr": "current topics"
+}, 
+{
+"msgid": "recently mentioned", 
+"msgstr": "recently mentioned"
+}, 
+{
+"msgid": "posts tagged", 
+"msgstr": "posts tagged"
+}, 
+{
+"msgid": "posts about", 
+"msgstr": "posts about"
+}, 
+{
+"msgid": "write a public post", 
+"msgstr": "write a public post"
+}, 
+{
+"msgid": "publish", 
+"msgstr": "publish"
+}, 
+{
+"msgid": "no messages", 
+"msgstr": "no messages"
+}, 
+{
+"msgid": "delete", 
+"msgstr": "delete"
+}, 
+{
+"msgid": "Before you can see profile pages, please fill in the basic data for your own profile page.", 
+"msgstr": "Before you can see profile pages, please fill in the basic data for your own profile page."
+}, 
+{
+"msgid": "Success!", 
+"msgstr": "Success!"
+}, 
+{
+"msgid": "Error!", 
+"msgstr": "Error!"
+}, 
+{
+"msgid": "set as main picture", 
+"msgstr": "set as main picture"
+}, 
+{
+"msgid": "delete picture", 
+"msgstr": "delete picture"
+}, 
+{
+"msgid": "Upload a picture you yourself!", 
+"msgstr": "Upload a picture you yourself!"
+}, 
+{
+"msgid": "You can upload up to 100 pictures of yourself.", 
+"msgstr": "You can upload up to 100 pictures of yourself."
+}, 
+{
+"msgid": "Some simple rules:", 
+"msgstr": "Some simple rules:"
+}, 
+{
+"msgid": "Please upload only pictures of you.", 
+"msgstr": "Please upload only pictures of you."
+}, 
+{
+"msgid": "Do not upload pictures of celebrities or other people.", 
+"msgstr": "Do not upload pictures of celebrities or other people."
+}, 
+{
+"msgid": "Do never upload pictures with violence or nudity.", 
+"msgstr": "Do never upload pictures with violence or nudity."
+}, 
+{
+"msgid": "I am a", 
+"msgstr": "I am a"
+}, 
+{
+"msgid": "date of birth", 
+"msgstr": "date of birth"
+}, 
+{
+"msgid": "Your birthdate is never shown in your profile! Only your age, and your occidental and oriental star signs are shown.", 
+"msgstr": "Your birthdate is never shown in your profile! Only your age, and your occidental and oriental star signs are shown."
+}, 
+{
+"msgid": "Find my location", 
+"msgstr": "Find my location"
+}, 
+{
+"msgid": "Click to automatically find the city closest to you, or manually select the city below.", 
+"msgstr": "Click to automatically find the city closest to you, or manually select the city below."
+}, 
+{
+"msgid": "your country", 
+"msgstr": "your country"
+}, 
+{
+"msgid": "your city", 
+"msgstr": "your city"
+}, 
+{
+"msgid": "publish changes", 
+"msgstr": "publish changes"
+}, 
+{
+"msgid": "mark all as read", 
+"msgstr": "mark all as read"
+}, 
+{
+"msgid": "mark unread", 
+"msgstr": "mark unread"
+}, 
+{
+"msgid": "mark as read", 
+"msgstr": "mark as read"
+}, 
+{
+"msgid": "block {0}", 
+"msgstr": "block {0}"
+}, 
+{
+"msgid": "Add you own CSS style to change the looks of your profile page.", 
+"msgstr": "Add you own CSS style to change the looks of your profile page."
+}, 
+{
+"msgid": "not found", 
+"msgstr": "not found"
+}, 
+{
+"msgid": "show interest", 
+"msgstr": "show interest"
+}, 
+{
+"msgid": "remove interest", 
+"msgstr": "remove interest"
+}, 
+{
+"msgid": "ask to be friends", 
+"msgstr": "ask to be friends"
+}, 
+{
+"msgid": "cancel friendship", 
+"msgstr": "cancel friendship"
+}, 
+{
+"msgid": "cancel friend request", 
+"msgstr": "cancel friend request"
+}, 
+{
+"msgid": "confirm friend request", 
+"msgstr": "confirm friend request"
+}, 
+{
+"msgid": "add to favorites", 
+"msgstr": "add to favorites"
+}, 
+{
+"msgid": "remove from favorites", 
+"msgstr": "remove from favorites"
+}, 
+{
+"msgid": "talk messages", 
+"msgstr": "talk messages"
+}, 
+{
+"msgid": "block", 
+"msgstr": "block"
+}, 
+{
+"msgid": "report", 
+"msgstr": "report"
+}, 
+{
+"msgid": "mod: delete user", 
+"msgstr": "mod: delete user"
+}, 
+{
+"msgid": "online now!", 
+"msgstr": "online now!"
+}, 
+{
+"msgid": "away", 
+"msgstr": "away"
+}, 
+{
+"msgid": "offline", 
+"msgstr": "offline"
+}, 
+{
+"msgid": "friend request sent", 
+"msgstr": "friend request sent"
+}, 
+{
+"msgid": "friend request received", 
+"msgstr": "friend request received"
+}, 
+{
+"msgid": "match", 
+"msgstr": "match"
+}, 
+{
+"msgid": "you are interested", 
+"msgstr": "you are interested"
+}, 
+{
+"msgid": "{0} is interested in you", 
+"msgstr": "{0} is interested in you"
+}, 
+{
+"msgid": "favorite", 
+"msgstr": "favorite"
+}, 
+{
+"msgid": "details", 
+"msgstr": "details"
+}, 
+{
+"msgid": "account created", 
+"msgstr": "account created"
+}, 
+{
+"msgid": "last active", 
+"msgstr": "last active"
+}, 
+{
+"msgid": "never", 
+"msgstr": "never"
+}, 
+{
+"msgid": "profile views", 
+"msgstr": "profile views"
+}, 
+{
+"msgid": "looking for", 
+"msgstr": "looking for"
+}, 
+{
+"msgid": "gender", 
+"msgstr": "gender"
+}, 
+{
+"msgid": "city", 
+"msgstr": "city"
+}, 
+{
+"msgid": "age", 
+"msgstr": "age"
+}, 
+{
+"msgid": "height", 
+"msgstr": "height"
+}, 
+{
+"msgid": "weight", 
+"msgstr": "weight"
+}, 
+{
+"msgid": "education", 
+"msgstr": "education"
+}, 
+{
+"msgid": "income", 
+"msgstr": "income"
+}, 
+{
+"msgid": "occupation", 
+"msgstr": "occupation"
+}, 
+{
+"msgid": "relationship status", 
+"msgstr": "relationship status"
+}, 
+{
+"msgid": "has children", 
+"msgstr": "has children"
+}, 
+{
+"msgid": "wants more children", 
+"msgstr": "wants more children"
+}, 
+{
+"msgid": "religion", 
+"msgstr": "religion"
+}, 
+{
+"msgid": "spirituality", 
+"msgstr": "spirituality"
+}, 
+{
+"msgid": "western zodiac", 
+"msgstr": "western zodiac"
+}, 
+{
+"msgid": "eastern zodiac", 
+"msgstr": "eastern zodiac"
+}, 
+{
+"msgid": "books", 
+"msgstr": "books"
+}, 
+{
+"msgid": "movies", 
+"msgstr": "movies"
+}, 
+{
+"msgid": "music", 
+"msgstr": "music"
+}, 
+{
+"msgid": "arts", 
+"msgstr": "arts"
+}, 
+{
+"msgid": "travel", 
+"msgstr": "travel"
+}, 
+{
+"msgid": "food", 
+"msgstr": "food"
+}, 
+{
+"msgid": "quotes", 
+"msgstr": "quotes"
+}, 
+{
+"msgid": "sports", 
+"msgstr": "sports"
+}, 
+{
+"msgid": "you blocked {0}!", 
+"msgstr": "you blocked {0}!"
+}, 
+{
+"msgid": "unblock", 
+"msgstr": "unblock"
+}, 
+{
+"msgid": "write a private message...", 
+"msgstr": "write a private message..."
+}, 
+{
+"msgid": "Offline? Try checking for new messages again...", 
+"msgstr": "Offline? Try checking for new messages again..."
+}, 
+{
+"msgid": "Error trying to send message.", 
+"msgstr": "Error trying to send message."
+}, 
+{
+"msgid": "Please try again.", 
+"msgstr": "Please try again."
+}, 
+{
+"msgid": "{1} friends of {0}", 
+"msgstr": "{1} friends of {0}"
+}, 
+{
+"msgid": "no friends", 
+"msgstr": "no friends"
+}, 
+{
+"msgid": "{0} does not have any friends yet.", 
+"msgstr": "{0} does not have any friends yet."
+}, 
+{
+"msgid": "change password", 
+"msgstr": "change password"
+}, 
+{
+"msgid": "change email address", 
+"msgstr": "change email address"
+}, 
+{
+"msgid": "logout", 
+"msgstr": "logout"
+}, 
+{
+"msgid": "old password", 
+"msgstr": "old password"
+}, 
+{
+"msgid": "Type in your old password first.", 
+"msgstr": "Type in your old password first."
+}, 
+{
+"msgid": "Please type in your old password.", 
+"msgstr": "Please type in your old password."
+}, 
+{
+"msgid": "new password", 
+"msgstr": "new password"
+}, 
+{
+"msgid": "new password again", 
+"msgstr": "new password again"
+}, 
+{
+"msgid": "Please type the same new password (at least 6 characters) into both fields.", 
+"msgstr": "Please type the same new password (at least 6 characters) into both fields."
+}, 
+{
+"msgid": "appearence stuff", 
+"msgstr": "appearence stuff"
+}, 
+{
+"msgid": "eyecolor", 
+"msgstr": "eyecolor"
+}, 
+{
+"msgid": "haircolor", 
+"msgstr": "haircolor"
+}, 
+{
+"msgid": "looks", 
+"msgstr": "looks"
+}, 
+{
+"msgid": "relationship stuff", 
+"msgstr": "relationship stuff"
+}, 
+{
+"msgid": "longest relationship", 
+"msgstr": "longest relationship"
+}, 
+{
+"msgid": "believes stuff", 
+"msgstr": "believes stuff"
+}, 
+{
+"msgid": "religiosity", 
+"msgstr": "religiosity"
+}, 
+{
+"msgid": "life stuff", 
+"msgstr": "life stuff"
+}, 
+{
+"msgid": "would you relocate", 
+"msgstr": "would you relocate"
+}, 
+{
+"msgid": "do you have children", 
+"msgstr": "do you have children"
+}, 
+{
+"msgid": "do you want (more) children", 
+"msgstr": "do you want (more) children"
+}, 
+{
+"msgid": "professional stuff", 
+"msgstr": "professional stuff"
+}, 
+{
+"msgid": "jobfield", 
+"msgstr": "jobfield"
+}, 
+{
+"msgid": "health stuff", 
+"msgstr": "health stuff"
+}, 
+{
+"msgid": "fitness", 
+"msgstr": "fitness"
+}, 
+{
+"msgid": "drugs stuff", 
+"msgstr": "drugs stuff"
+}, 
+{
+"msgid": "alcohol", 
+"msgstr": "alcohol"
+}, 
+{
+"msgid": "tobacco", 
+"msgstr": "tobacco"
+}, 
+{
+"msgid": "marijuana (where legal)", 
+"msgstr": "marijuana (where legal)"
+}, 
+{
+"msgid": "tell people about yourself", 
+"msgstr": "tell people about yourself"
+}, 
+{
+"msgid": "your favourite books", 
+"msgstr": "your favourite books"
+}, 
+{
+"msgid": "your favourite movies", 
+"msgstr": "your favourite movies"
+}, 
+{
+"msgid": "your favourite music", 
+"msgstr": "your favourite music"
+}, 
+{
+"msgid": "your favourite arts", 
+"msgstr": "your favourite arts"
+}, 
+{
+"msgid": "your favourite travel", 
+"msgstr": "your favourite travel"
+}, 
+{
+"msgid": "your favourite food", 
+"msgstr": "your favourite food"
+}, 
+{
+"msgid": "your favourite quotes", 
+"msgstr": "your favourite quotes"
+}, 
+{
+"msgid": "your favourite sports", 
+"msgstr": "your favourite sports"
+}, 
+{
+"msgid": "basics", 
+"msgstr": "basics"
+}, 
+{
+"msgid": "photos", 
+"msgstr": "photos"
+}, 
+{
+"msgid": "design", 
+"msgstr": "design"
+}, 
+{
+"msgid": "account", 
+"msgstr": "account"
+}, 
+{
+"msgid": "security", 
+"msgstr": "security"
+}, 
+{
+"msgid": "interested in you", 
+"msgstr": "interested in you"
+}, 
+{
+"msgid": "interest sent", 
+"msgstr": "interest sent"
+}, 
+{
+"msgid": "friend invites received", 
+"msgstr": "friend invites received"
+}, 
+{
+"msgid": "friend requests sent", 
+"msgstr": "friend requests sent"
+}, 
+{
+"msgid": "see what your matches are talking about", 
+"msgstr": "see what your matches are talking about"
+}, 
+{
+"msgid": "they are interested in you", 
+"msgstr": "they are interested in you"
+}, 
+{
+"msgid": "people you are interested in, wait for them to answer", 
+"msgstr": "people you are interested in, wait for them to answer"
+}, 
+{
+"msgid": "see what your friends are talking about", 
+"msgstr": "see what your friends are talking about"
+}, 
+{
+"msgid": "answer these friend invites and make new friends", 
+"msgstr": "answer these friend invites and make new friends"
+}, 
+{
+"msgid": "friend requests you sent that have not yet been answered", 
+"msgstr": "friend requests you sent that have not yet been answered"
+}, 
+{
+"msgid": "try checking for new messages again", 
+"msgstr": "try checking for new messages again"
+}, 
+{
+"msgid": "nobody here", 
+"msgstr": "nobody here"
+}
+]
---
+++ js/tr-language-es.js
@@ -0,0 +1,678 @@
+window.TR_LANGUAGE = [
+{
+"msgid": "find people", 
+"msgstr": "encontrar gente"
+}, 
+{
+"msgid": "talk", 
+"msgstr": "plática"
+}, 
+{
+"msgid": "inbox messages", 
+"msgstr": "mensajes inbox"
+}, 
+{
+"msgid": "new inbox messages", 
+"msgstr": "nuevos mensajes inbox"
+}, 
+{
+"msgid": "matches", 
+"msgstr": "matches"
+}, 
+{
+"msgid": "friends", 
+"msgstr": "amigos"
+}, 
+{
+"msgid": "viewed you", 
+"msgstr": "vio tu perfil"
+}, 
+{
+"msgid": "your favorites", 
+"msgstr": "tus favoritos"
+}, 
+{
+"msgid": "blocked", 
+"msgstr": "bloqueados"
+}, 
+{
+"msgid": "pictures", 
+"msgstr": "fotos"
+}, 
+{
+"msgid": "view profile", 
+"msgstr": "ver perfil"
+}, 
+{
+"msgid": "All rights reserved.", 
+"msgstr": "Todos los derechos reservados."
+}, 
+{
+"msgid": "site rules", 
+"msgstr": "regals del sitio"
+}, 
+{
+"msgid": "privacy", 
+"msgstr": "privacidad"
+}, 
+{
+"msgid": "by all", 
+"msgstr": "todos"
+}, 
+{
+"msgid": "by matches", 
+"msgstr": "matches"
+}, 
+{
+"msgid": "by friends", 
+"msgstr": "amigos"
+}, 
+{
+"msgid": "about me", 
+"msgstr": "yo"
+}, 
+{
+"msgid": "unread", 
+"msgstr": "nuevos"
+}, 
+{
+"msgid": "inbox", 
+"msgstr": "inbox"
+}, 
+{
+"msgid": "sent", 
+"msgstr": "enviado"
+}, 
+{
+"msgid": "nobody there", 
+"msgstr": "no hay nadie mas"
+}, 
+{
+"msgid": "current topics", 
+"msgstr": "temas actuales"
+}, 
+{
+"msgid": "recently mentioned", 
+"msgstr": "mencionado recientemente"
+}, 
+{
+"msgid": "posts tagged", 
+"msgstr": "posts tagged"
+}, 
+{
+"msgid": "posts about", 
+"msgstr": "posts sobre"
+}, 
+{
+"msgid": "write a public post", 
+"msgstr": "escribe un post público"
+}, 
+{
+"msgid": "publish", 
+"msgstr": "publicar"
+}, 
+{
+"msgid": "no messages", 
+"msgstr": "no hay mensajes"
+}, 
+{
+"msgid": "delete", 
+"msgstr": "borrar"
+}, 
+{
+"msgid": "Before you can see profile pages, please fill in the basic data for your own profile page.", 
+"msgstr": "Para ver perfiles de otros, por favor completa los datos básicos de tu página."
+}, 
+{
+"msgid": "Success!", 
+"msgstr": "¡Éxito!"
+}, 
+{
+"msgid": "Error!", 
+"msgstr": "¡Error!"
+}, 
+{
+"msgid": "set as main picture", 
+"msgstr": "foto principal"
+}, 
+{
+"msgid": "delete picture", 
+"msgstr": "borrar foto"
+}, 
+{
+"msgid": "Upload a picture you yourself!", 
+"msgstr": "¡Subir una foto de ti!"
+}, 
+{
+"msgid": "You can upload up to 100 pictures of yourself.", 
+"msgstr": "Puedes tener hasta 100 fotos de ti en tu página de perfil."
+}, 
+{
+"msgid": "Some simple rules:", 
+"msgstr": "Unas reglas sencillas:"
+}, 
+{
+"msgid": "Please upload only pictures of you.", 
+"msgstr": "Por favor, solo publica fotos de tí mismo/a."
+}, 
+{
+"msgid": "Do not upload pictures of celebrities or other people.", 
+"msgstr": "No publiques fotos de celebridades u otras personas."
+}, 
+{
+"msgid": "Do never upload pictures with violence or nudity.", 
+"msgstr": "No publiques fotos con violencia o desnudez."
+}, 
+{
+"msgid": "I am a", 
+"msgstr": "Soy"
+}, 
+{
+"msgid": "date of birth", 
+"msgstr": "fecha de nacimiento"
+}, 
+{
+"msgid": "Your birthdate is never shown in your profile! Only your age, and your occidental and oriental star signs are shown.", 
+"msgstr": "¡Tu fecha de nacimiento no se publica nunca en tu perfil! Unicamente se utiliza para publicar tu edad, y tus signos zodiacales occidental y oriental."
+}, 
+{
+"msgid": "Find my location", 
+"msgstr": "Encuentra mi ciudad"
+}, 
+{
+"msgid": "Click to automatically find the city closest to you, or manually select the city below.", 
+"msgstr": "Haz clic para automáticamente establecer tu ciudadad. También la puedes seleccionar manualmente."
+}, 
+{
+"msgid": "your country", 
+"msgstr": "tu país"
+}, 
+{
+"msgid": "your city", 
+"msgstr": "tu ciudad"
+}, 
+{
+"msgid": "publish changes", 
+"msgstr": "publicar"
+}, 
+{
+"msgid": "mark all as read", 
+"msgstr": "marcar todos como leidos"
+}, 
+{
+"msgid": "mark unread", 
+"msgstr": "marcar todos como nuevos"
+}, 
+{
+"msgid": "mark as read", 
+"msgstr": "marcar como leido"
+}, 
+{
+"msgid": "block {0}", 
+"msgstr": "bloquear {0}"
+}, 
+{
+"msgid": "Add you own CSS style to change the looks of your profile page.", 
+"msgstr": "Agrégale tu propio CSS para cambiar el diseño de tu página de perfil."
+}, 
+{
+"msgid": "not found", 
+"msgstr": "no encontrado"
+}, 
+{
+"msgid": "show interest", 
+"msgstr": "muestra interés"
+}, 
+{
+"msgid": "remove interest", 
+"msgstr": "borrar interés"
+}, 
+{
+"msgid": "ask to be friends", 
+"msgstr": "invita ser amigos"
+}, 
+{
+"msgid": "cancel friendship", 
+"msgstr": "borrar amistad"
+}, 
+{
+"msgid": "cancel friend request", 
+"msgstr": "borrar invitación de amistad"
+}, 
+{
+"msgid": "confirm friend request", 
+"msgstr": "confirmar amistad"
+}, 
+{
+"msgid": "add to favorites", 
+"msgstr": "a tus favoritos"
+}, 
+{
+"msgid": "remove from favorites", 
+"msgstr": "borrar de favoritos"
+}, 
+{
+"msgid": "talk messages", 
+"msgstr": "ver messages"
+}, 
+{
+"msgid": "block", 
+"msgstr": "bloquear"
+}, 
+{
+"msgid": "report", 
+"msgstr": "reportar"
+}, 
+{
+"msgid": "mod: delete user", 
+"msgstr": "mod: borrar usuario"
+}, 
+{
+"msgid": "online now!", 
+"msgstr": "¡en linea!"
+}, 
+{
+"msgid": "away", 
+"msgstr": "ausente"
+}, 
+{
+"msgid": "offline", 
+"msgstr": "offline"
+}, 
+{
+"msgid": "friend request sent", 
+"msgstr": "invitación de amistad enviada"
+}, 
+{
+"msgid": "friend request received", 
+"msgstr": "invitación de amistad recibida"
+}, 
+{
+"msgid": "match", 
+"msgstr": "match"
+}, 
+{
+"msgid": "you are interested", 
+"msgstr": "te interesa"
+}, 
+{
+"msgid": "{0} is interested in you", 
+"msgstr": "le interesas a {0}"
+}, 
+{
+"msgid": "favorite", 
+"msgstr": "en tus favoritos"
+}, 
+{
+"msgid": "details", 
+"msgstr": "detailes"
+}, 
+{
+"msgid": "account created", 
+"msgstr": "cuenta creada"
+}, 
+{
+"msgid": "last active", 
+"msgstr": "última actividad"
+}, 
+{
+"msgid": "never", 
+"msgstr": "nunca"
+}, 
+{
+"msgid": "profile views", 
+"msgstr": "visitas de perfil"
+}, 
+{
+"msgid": "looking for", 
+"msgstr": "buscando"
+}, 
+{
+"msgid": "gender", 
+"msgstr": "sexo"
+}, 
+{
+"msgid": "city", 
+"msgstr": "ciudad"
+}, 
+{
+"msgid": "age", 
+"msgstr": "edad"
+}, 
+{
+"msgid": "height", 
+"msgstr": "altura"
+}, 
+{
+"msgid": "weight", 
+"msgstr": "peso"
+}, 
+{
+"msgid": "education", 
+"msgstr": "educación"
+}, 
+{
+"msgid": "income", 
+"msgstr": "ingresos"
+}, 
+{
+"msgid": "occupation", 
+"msgstr": "occupación"
+}, 
+{
+"msgid": "relationship status", 
+"msgstr": "estado civil"
+}, 
+{
+"msgid": "has children", 
+"msgstr": "tiene hijos"
+}, 
+{
+"msgid": "wants more children", 
+"msgstr": "quiere mas hijos"
+}, 
+{
+"msgid": "religion", 
+"msgstr": "religión"
+}, 
+{
+"msgid": "spirituality", 
+"msgstr": "espiritualidad"
+}, 
+{
+"msgid": "western zodiac", 
+"msgstr": "zodiaco occidental"
+}, 
+{
+"msgid": "eastern zodiac", 
+"msgstr": "zodiaco oriental"
+}, 
+{
+"msgid": "books", 
+"msgstr": "libros"
+}, 
+{
+"msgid": "movies", 
+"msgstr": "películas"
+}, 
+{
+"msgid": "music", 
+"msgstr": "música"
+}, 
+{
+"msgid": "arts", 
+"msgstr": "artes"
+}, 
+{
+"msgid": "travel", 
+"msgstr": "viajes"
+}, 
+{
+"msgid": "food", 
+"msgstr": "comida"
+}, 
+{
+"msgid": "quotes", 
+"msgstr": "citas"
+}, 
+{
+"msgid": "sports", 
+"msgstr": "deportes"
+}, 
+{
+"msgid": "you blocked {0}!", 
+"msgstr": "¡bloqueaste a {0}!"
+}, 
+{
+"msgid": "unblock", 
+"msgstr": "desbloquear"
+}, 
+{
+"msgid": "write a private message...", 
+"msgstr": "escribe un mensaje privado..."
+}, 
+{
+"msgid": "Offline? Try checking for new messages again...", 
+"msgstr": "Offline? Intente ver si hay mensajes de nuevo..."
+}, 
+{
+"msgid": "Error trying to send message.", 
+"msgstr": "Error al intentar enviar el mensaje."
+}, 
+{
+"msgid": "Please try again.", 
+"msgstr": "Por favor, inténtelo de nuevo."
+}, 
+{
+"msgid": "{1} friends of {0}", 
+"msgstr": "{1} amigos de {0}"
+}, 
+{
+"msgid": "no friends", 
+"msgstr": "sin amigos"
+}, 
+{
+"msgid": "{0} does not have any friends yet.", 
+"msgstr": "{0} todavía no tiene amigos."
+}, 
+{
+"msgid": "change password", 
+"msgstr": "cambiar contraseña"
+}, 
+{
+"msgid": "change email address", 
+"msgstr": "cambiar dirección email"
+}, 
+{
+"msgid": "logout", 
+"msgstr": "logout"
+}, 
+{
+"msgid": "old password", 
+"msgstr": "contraseña actual"
+}, 
+{
+"msgid": "Type in your old password first.", 
+"msgstr": "Primero, escribe tu contraseña actual."
+}, 
+{
+"msgid": "Please type in your old password.", 
+"msgstr": "Por favor escribe tu contraseña actual."
+}, 
+{
+"msgid": "new password", 
+"msgstr": "contraseña nueva"
+}, 
+{
+"msgid": "new password again", 
+"msgstr": "contraseña nueva otra vez"
+}, 
+{
+"msgid": "Please type the same new password (at least 6 characters) into both fields.", 
+"msgstr": "Por favor, escribe la misma contraseña nueva (por lo menos 6 caracteres) en ambos campos."
+}, 
+{
+"msgid": "appearence stuff", 
+"msgstr": "cosas de apariencia"
+}, 
+{
+"msgid": "eyecolor", 
+"msgstr": "color de ojo"
+}, 
+{
+"msgid": "haircolor", 
+"msgstr": "color de cabello"
+}, 
+{
+"msgid": "looks", 
+"msgstr": "apariencia"
+}, 
+{
+"msgid": "relationship stuff", 
+"msgstr": "coses de relaciones"
+}, 
+{
+"msgid": "longest relationship", 
+"msgstr": "relación más larga"
+}, 
+{
+"msgid": "believes stuff", 
+"msgstr": "cosas de creencia"
+}, 
+{
+"msgid": "religiosity", 
+"msgstr": "religiosidad"
+}, 
+{
+"msgid": "life stuff", 
+"msgstr": "cosas de la vida"
+}, 
+{
+"msgid": "would you relocate", 
+"msgstr": "te mudarias a otra ciudad"
+}, 
+{
+"msgid": "do you have children", 
+"msgstr": "tienes hijos"
+}, 
+{
+"msgid": "do you want (more) children", 
+"msgstr": "quiere (mas) hijos"
+}, 
+{
+"msgid": "professional stuff", 
+"msgstr": "cosas del trabajo"
+}, 
+{
+"msgid": "jobfield", 
+"msgstr": "campo laboral"
+}, 
+{
+"msgid": "health stuff", 
+"msgstr": "cosas de salud"
+}, 
+{
+"msgid": "fitness", 
+"msgstr": "estado físico"
+}, 
+{
+"msgid": "drugs stuff", 
+"msgstr": "cosas de drogas"
+}, 
+{
+"msgid": "alcohol", 
+"msgstr": "alcohol"
+}, 
+{
+"msgid": "tobacco", 
+"msgstr": "tabaco"
+}, 
+{
+"msgid": "marijuana (where legal)", 
+"msgstr": "marijuana (donde es legal)"
+}, 
+{
+"msgid": "tell people about yourself", 
+"msgstr": "escribe algunas frases acerca de tí"
+}, 
+{
+"msgid": "your favourite books", 
+"msgstr": "tus libros favoritos"
+}, 
+{
+"msgid": "your favourite movies", 
+"msgstr": "tus películas favoritas"
+}, 
+{
+"msgid": "your favourite music", 
+"msgstr": "tus música favorita"
+}, 
+{
+"msgid": "your favourite arts", 
+"msgstr": "tus artes favoritas"
+}, 
+{
+"msgid": "your favourite travel", 
+"msgstr": "tus viajes favoritas"
+}, 
+{
+"msgid": "your favourite food", 
+"msgstr": "tus comidas favoritas"
+}, 
+{
+"msgid": "your favourite quotes", 
+"msgstr": "tus citas favoritas"
+}, 
+{
+"msgid": "your favourite sports", 
+"msgstr": "tus deportes favoritas"
+}, 
+{
+"msgid": "basics", 
+"msgstr": "básicos"
+}, 
+{
+"msgid": "photos", 
+"msgstr": "fotos"
+}, 
+{
+"msgid": "design", 
+"msgstr": "diseño"
+}, 
+{
+"msgid": "account", 
+"msgstr": "cuenta"
+}, 
+{
+"msgid": "security", 
+"msgstr": "seguridad"
+}, 
+{
+"msgid": "interested in you", 
+"msgstr": "le interesas"
+}, 
+{
+"msgid": "interest sent", 
+"msgstr": "interés enviado"
+}, 
+{
+"msgid": "friend invites received", 
+"msgstr": "invitaciones de amistad recibidas"
+}, 
+{
+"msgid": "friend requests sent", 
+"msgstr": "invitaciones de amistad enviadas"
+}, 
+{
+"msgid": "see what your matches are talking about", 
+"msgstr": "ver los mensajes públicos de todos tus matches"
+}, 
+{
+"msgid": "they are interested in you", 
+"msgstr": "están interesados en ti"
+}, 
+{
+"msgid": "people you are interested in, wait for them to answer", 
+"msgstr": "te interesan, espera a que respondan"
+}, 
+{
+"msgid": "see what your friends are talking about", 
+"msgstr": "ver los mensajes públicos de todos tus amigos"
+}, 
+{
+"msgid": "answer these friend invites and make new friends", 
+"msgstr": "responde a estas invitaciones de amistad y haz nuevos amigos"
+}, 
+{
+"msgid": "friend requests you sent that have not yet been answered", 
+"msgstr": "invitaciones de amistad que enviaste y que aún no han sido respondidas"
+}, 
+{
+"msgid": "try checking for new messages again", 
+"msgstr": "intenta cargar nuevos mensajes otra vez"
+}, 
+{
+"msgid": "nobody here", 
+"msgstr": "no hay nadie"
+}
+]
