79cfd8c00016c71a1697c3d230bef394ccc2cf4f
Christian Steimel <me@C14L.com>
2016-09-27 00:21:56 +0200

    Fix gulp tasks, combine and uglify assets, load fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var del = require('del');
 var gulp = require('gulp');
 var gulpIf = require('gulp-if');
 var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
 var useref = require('gulp-useref');
 var runSequence = require('run-sequence');
 
@@ -10,30 +11,30 @@ gulp.task('clean:dist', function(){
     return del.sync('dist');
 });
 
-gulp.task('copymodules', function(){
-    const li = [
-        "node_modules/angular/angular.min.js*",
-        "node_modules/angular-route/angular-route.min.js*",
-        "node_modules/angular-resource/angular-resource.min.js*",
-        "node_modules/angular-sanitize/angular-sanitize.min.js*",
-        "node_modules/angular-upload/angular-upload.min.js*",
-        "node_modules/angular-bindonce/bindonce.min.js*",
-    ];
-    return gulp.src(li).pipe(gulp.dest('dist/app/vendor'));
-});
-
-gulp.task('collect', function(){
+gulp.task('collect_assets', function(){
     return gulp.src('src/app/index.html')
         .pipe(useref())
-        //.pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('copyall', function(){
-    return gulp.src('src/app/**/*')
-        .pipe(gulp.dest('dist/app'));
+gulp.task('copy_tpls', function(){
+    const files = [
+        'src/app/**/*.html', 
+        'src/app/**/*.json', 
+    ];
+    return gulp.src(files).pipe(gulp.dest('dist/app'));
+});
+
+gulp.task('copy_font', function(){
+    files = [
+        'src/app/utils/themify-icons/fonts/themify.ttf',
+        'src/app/utils/themify-icons/fonts/themify.woff',
+    ];
+    return gulp.src(files).pipe(gulp.dest('dist/app/fonts'))
 });
 
 gulp.task('default', function(callback){
-    runSequence('clean:dist', 'copymodules', 'copyall', 'collect', callback);
+    runSequence('clean:dist', 'copy_font', 'copy_tpls', 'collect_assets', callback);
 });
